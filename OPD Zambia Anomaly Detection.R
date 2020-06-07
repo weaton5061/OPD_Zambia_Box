9d573bb7ba4fc51aa51602db8d340e56f408e46f
weaton5061 <weaton5061@gmail.com>
2020-06-06 20:17:08 -0500

    commit and try and run with Matt's loop

--- OPD Zambia Anomaly Detection.R
+++ OPD Zambia Anomaly Detection.R
@@ -3,8 +3,8 @@
 # Purpose: Zambia DiD analysis of PAMO project iCCM impact on malaria IPD and/or death (Luapula, Northern, Muchinga)
 # Notes: Ruth requests concentrating on 6 Provinces: Luapula, Muchinga, Northern, Eastern, Northwestern, Western
 #        n = approx 70 districts total
-# Created: 2020-06-04
-# Last updated: 
+# Created: 06-04-2020
+# Last updated: 06-06-2020
 # Status: in progress
 # Notes: Editing for OPD anomolies 
 
@@ -17,7 +17,6 @@
 # 6) Produce Figures
 # 7) Produce Tables
 
-# -------------------------------------------------------------------------------------------------------------------------------------
 # Remove all objects form the current workspace
 
 rm(list = ls())
@@ -39,15 +38,12 @@ library(dplyr)
 ## ---- Set Working Drive -------------------------------------------------------------------------------------
 setwd("/Users/willeaton/Box/OPD Cleaning/OPD Zambia Project Cloned Git Repository/OPD_Zambia_Project_Box")
 
-## ---- import -------------------------------------------------------------------------------------
-## Import datasets
-
-## ---- Inpatient Outpatient Facility Data  -------------------------------------------------------------------------------------
+## ---- Import Inpatient Outpatient Facility Data  -------------------------------------------------------------------------------------
 inpatient_outpatient.df <- read.csv("/Users/willeaton/Box/OPD Cleaning/inpatient_outpatient_facility_data.csv")
 
-## ---- subset to appropriate 6 Provinces  -------------------------------------------------------------------------------
+## ---- subset to appropriate n= 6 Provinces  -------------------------------------------------------------------------------
 #  ---- Luapula, Muchinga, Northern, Eastern, Northwestern, Western ------------------------------------------------------
-#  ----n = 68 districts, over 1000 health facilities
+#  ----n = 68 Districts, over 1000 health facilities
 sixprov.df <- inpatient_outpatient.df %>%
     filter(Province == "Luapula Province" | Province ==  "Muchinga Province" | Province == 'Northern Province' | 
                Province == 'Eastern Province' | Province == 'North Western Province' | Province ==  'Western Province')
@@ -60,8 +56,17 @@ sixprov.df <- inpatient_outpatient.df %>%
 #               [7] lu Mukamba Rural Health Centre  
 # Test out anomalize package on oneprov
 oneprov.df <- inpatient_outpatient.df %>%
-    filter(Province == "Luapula Province" & District =="Mwansabombwe District") # &
-           #Org_Unit == "lu Mbereshi Mission Hospital Affiliated Health Centre")
+    filter(Province == "Luapula Province" & District =="Mwansabombwe District" &
+               Org_Unit == "lu Mbereshi Mission Hospital Affiliated Health Centre")
+
+# create health facility dataset, n = 
+hf <- subset(sixprov.df, select = c(Province, District, Org_Unit))
+hf_names<-distinct(hf)
+# sort health facilities by multiple columns
+hf_names_2 <- hf_names[
+    with(hf_names, order(Province, District)),
+    ]
+
 
 # Note: in_out_pt_6_pro.df has n = 463,739 OPD First Attendance observations
 # Inpatient Admissions OPD First Attendance                 <NA> 
@@ -84,37 +89,24 @@ unique(outpatient.df$Value, useNA = "always")
 
 #attempt to change factors to character variables
 # is this necessary?
-outpatient.df %>% mutate_if(is.factor, as.character) -> outpatient.df
-typeof(mydata$Province)
-typeof(mydata$Org_Unit)
+# outpatient.df %>% mutate_if(is.factor, as.character) -> outpatient.df
+
 
 #Aggregate Value Data by the HF, Month and Year
 #outpatient1 <- outpatient.df %>% group_by(Year, Month, Org_Unit, District) %>% summarize(Value = sum(Value))
 # commenting this out for now (on 6-4-20) so that Province is also included in dataframe
 outpatient2 <- outpatient.df %>% group_by(Year, Month, Org_Unit, District, Province) %>% summarize(Total_Value = sum(Value))
 
-# ------ Toying around with group_by
-outpatient2 <- outpatient.df %>% group_by(Year, Month, Org_Unit, District, Province) %>% summarize(Total_Value = sum(Value))
-
-outpatient2 %>%
-    group_by(Province) %>%
-    group_by(District) %>% 
-    group_by(Org_Unit) %>% 
-    group_by(Year) %>% 
-    group_by(Month) %>% 
-    group_map(~ head(.x, 2L))
-#------
-
 #Create Date from Month, Year
 outpatient2$month_num <- match(outpatient2$Month, month.abb)
 outpatient2$date <- as.Date(with(outpatient2, paste(Year, month_num, 1, sep="-")), "%Y-%m-%d")
 
 #Identify all Unique Values for District
-Districts.df <- data.frame(unique(outpatient2$District)) # n = 68 unique districts
-order("unique.outpatient2.District.") #is this producing what it is supposed to?
-
-# Note: not sure what this code does?
-Districts.df[,order("unique.outpatient2.District."(df))]
+# Districts.df <- data.frame(unique(outpatient2$District)) # n = 68 unique districts
+# order("unique.outpatient2.District.") #is this producing what it is supposed to?
+# 
+# # Note: not sure what this code does?
+# Districts.df[,order("unique.outpatient2.District."(df))]
 
 # ---- write to csv file
 # write.csv(outpatient2, file = "/Users/willeaton/Box/OPD Cleaning/OPD Zambia Project Cloned Git Repository/OPD_Zambia_Project_Box/OPD_Zambia_R_Working_Drive/csv/outpatient2.csv")
@@ -132,29 +124,27 @@ Districts.df[,order("unique.outpatient2.District."(df))]
 # [57] Sioma District        Limulunga District    Luampa District       Mitete District       Mafinga District      Lavushimanda District Kanchibiya District  
 # [64] Mongu District        Shang'ombo District   Chadiza District      Chembe District       Nkeyema District   
 
-# this produces faceted time series line plot
-outpatient2 %>%
-    filter(District=="Kaoma District") %>%
-    ggplot() +
-    geom_line(mapping = (aes(x=date, y=Total_Value))) +
-    facet_wrap(~ Org_Unit, scales="free_y") + theme(strip.text = element_text(size=8))
+# -- this produces faceted time series line plot --
+# outpatient2 %>%
+#     filter(District=="Mwansabombwe District") %>%
+#     ggplot() +
+#     geom_line(mapping = (aes(x=date, y=Total_Value))) +
+#     facet_wrap(~ Org_Unit, scales="free_y") + theme(strip.text = element_text(size=8))
 
 
 # Analyze Outliers --------------------------------------------------------
-# Alternative code for assessing outliers ---------------------------------
-
-# try deleting the year, month, and month num variables
 
 # How to delete first, second and seventh column
 mydata <- outpatient2
 
 # Drop the columns of the dataframe
-mydata <- subset(outpatient2, select = c(Province, District, Org_Unit, Total_Value, date)) # Province, District, Org_Unit
+mydata <- subset(outpatient2, select = c(Province, Total_Value, date)) # Province, District, Org_Unit
 
 # sort by date
 mydata2<-mydata[order(as.Date(mydata$date, format = "%d/%m/%Y")),]
 
 # Try Twitter and GESD Method ---------------------------------------------
+
 mydata2 %>%    # Twitter and GESD
     time_decompose(Total_Value, method = "twitter", # The time series decomposition method. One of "stl" or "twitter". The
                    # STL method uses seasonal decomposition (see decompose_stl()). The Twitter
@@ -166,103 +156,105 @@ mydata2 %>%    # Twitter and GESD
                    merge = TRUE) %>%  # A boolean. FALSE by default. If TRUE, will append results to the original data.
     anomalize(remainder, method = "gesd", 
               alpha = 0.05,               # We can decrease alpha, which increases the bands making it more difficult to be an outlier. In reality, you’ll probably want to leave alpha in the range of 0.10 to 0.02, but it makes a nice illustration of how you can also use max_anoms to ensure only the most aggregious anomalies are identified.
-              max_anoms = 0.20) %>%        # the maximum percentage of data that can be an anomaly
-    time_recompose() %>% 
-    #Anomaly Visualization
-    #plot_anomalies(ncol = 3, alpha_dots = 0.25) # this line of code plots without the grey area
-    plot_anomalies(time_recomposed = TRUE) #+ #, ncol = 3, alpha_dots = 0.25)
-    labs(title = "Time seriesd data - Twitter + GESD Method", x = "Time",
-         y = "Total outpatient visits", subtitle = "insert subtitle") 
-
-# try facet with mydata 2 twitter method
-# this produces faceted time series line plot
-mydata2 %>%
-    filter(District=="Mwansabombwe District") %>%
-    time_decompose(Total_Value, method = "twitter", # The time series decomposition method. One of "stl" or "twitter". The
-                   # STL method uses seasonal decomposition (see decompose_stl()). The Twitter
-                   # method uses trend to remove the trend (see decompose_twitter()).
-                   frequency = "auto", # Controls the seasonal adjustment (removal of seasonality). Input can be either "auto", a time-based definition (e.g. "2 weeks"), or a numeric number of obser- vations per frequency (e.g.)
-                   # Consider frequency = "auto" or 12, however obtain more anomalies if 12
-                   trend = "auto", # Controls the trend component For stl, the trend controls the sensitivity of the lowess smoother, which is used to remove the remainder. 
-                   # For twitter, the trend controls the period width of the median, which are used to remove the trend and center the remainder.
-                   merge = TRUE) %>%  # A boolean. FALSE by default. If TRUE, will append results to the original data.
-    anomalize(remainder, method = "gesd", 
-              alpha = 0.05,               # We can decrease alpha, which increases the bands making it more difficult to be an outlier. In reality, you’ll probably want to leave alpha in the range of 0.10 to 0.02, but it makes a nice illustration of how you can also use max_anoms to ensure only the most aggregious anomalies are identified.
-              max_anoms = 0.20) %>%        # the maximum percentage of data that can be an anomaly
+              max_anoms = 0.20,           # the maximum percentage of data that can be an anomaly
+              verbose = TRUE) %>%         # return a report of useful information related to the outliers
     time_recompose() %>% 
     #Anomaly Visualization
     #plot_anomalies(ncol = 3, alpha_dots = 0.25) # this line of code plots without the grey area
     plot_anomalies(time_recomposed = TRUE) + #, ncol = 3, alpha_dots = 0.25)
     labs(title = "Time seriesd data - Twitter + GESD Method", x = "Time",
-         y = "Total outpatient visits", subtitle = "insert subtitle") #+
-    facet_wrap(~ Org_Unit, scales="free_y") + theme(strip.text = element_text(size=8))
+         y = "Total outpatient visits", subtitle = "insert subtitle") 
+
 
 # details
-mydata2 %>% glimpse()
+# mydata2 %>% glimpse()
 
 # plot_anomaly_decomposition() for visualizing the inner workings of how 
 # algorithm detects anomalies in the “remainder”.
-mydata2 %>%
-    time_decompose(Total_Value, frequency = "auto", trend = "auto", method = "twitter", merge = TRUE) %>%  # consider frequency = 12, however obtain more anomalies
-    anomalize(remainder, method = "gesd") %>% 
-    time_recompose() %>% 
-    plot_anomaly_decomposition() +
-    ggtitle("Freq/Trend = 'auto'")
+# mydata2 %>%
+#     time_decompose(Total_Value, frequency = "auto", trend = "auto", method = "twitter", merge = TRUE) %>%  # consider frequency = 12, however obtain more anomalies
+#     anomalize(remainder, method = "gesd") %>% 
+#     time_recompose() %>% 
+#     plot_anomaly_decomposition() +
+#     ggtitle("Freq/Trend = 'auto'")
 
 
 # Plot anomalies in multiple time series ----------------------------------
 
 #### MULTIPLE TIME SERIES ####
-mydata2 %>%
-    time_decompose(Total_Value, method = "stl") %>%
-    anomalize(remainder, method = "iqr") %>%
-    time_recompose() %>%
-    plot_anomalies(time_recomposed = TRUE, ncol = 3)
-
-plot_anomalies(mydata2, time_recomposed = FALSE, ncol = 1,
-               color_no = "#2c3e50", color_yes = "#e31a1c",
-               fill_ribbon = "grey70", alpha_dots = 1, alpha_circles = 1,
-               alpha_ribbon = 1, size_dots = 1.5, size_circles = 4)
-
+# mydata2 %>%
+#     time_decompose(Total_Value, method = "stl") %>%
+#     anomalize(remainder, method = "iqr") %>%
+#     time_recompose() %>%
+#     plot_anomalies(time_recomposed = TRUE, ncol = 3)
+# 
+# plot_anomalies(mydata2, time_recomposed = FALSE, ncol = 1,
+#                color_no = "#2c3e50", color_yes = "#e31a1c",
+#                fill_ribbon = "grey70", alpha_dots = 1, alpha_circles = 1,
+#                alpha_ribbon = 1, size_dots = 1.5, size_circles = 4)
 
 # Yet another method of examining outliers ----------------------------------
 # Analyze outliers: Outlier Report is available with verbose = TRUE
-iqr_outliers <- iqr(x, alpha = 0.05, max_anoms = 0.2, verbose = TRUE)$outlier_report
-
-gesd_outliers <- gesd(x, alpha = 0.05, max_anoms = 0.2, verbose = TRUE)$outlier_report
-
-# ploting function for anomaly plots
-ggsetup <- function(data) {
-    data %>%
-        ggplot(aes(rank, value, color = outlier)) +
-        geom_point() +
-        geom_line(aes(y = limit_upper), color = "red", linetype = 2) +
-        geom_line(aes(y = limit_lower), color = "red", linetype = 2) +
-        geom_text(aes(label = index), vjust = -1.25) +
-        theme_bw() +
-        scale_color_manual(values = c("No" = "#2c3e50", "Yes" = "#e31a1c")) +
-        expand_limits(y = 13) +
-        theme(legend.position = "bottom")
-}
+# iqr_outliers <- iqr(x, alpha = 0.05, max_anoms = 0.2, verbose = TRUE)$outlier_report
+# 
+# gesd_outliers <- gesd(x, alpha = 0.05, max_anoms = 0.2, verbose = TRUE)$outlier_report
+# 
+# # ploting function for anomaly plots
+# ggsetup <- function(data) {
+#     data %>%
+#         ggplot(aes(rank, value, color = outlier)) +
+#         geom_point() +
+#         geom_line(aes(y = limit_upper), color = "red", linetype = 2) +
+#         geom_line(aes(y = limit_lower), color = "red", linetype = 2) +
+#         geom_text(aes(label = index), vjust = -1.25) +
+#         theme_bw() +
+#         scale_color_manual(values = c("No" = "#2c3e50", "Yes" = "#e31a1c")) +
+#         expand_limits(y = 13) +
+#         theme(legend.position = "bottom")
+# }
 
 
 # Visualize
-p3 <- iqr_outliers %>% 
-    ggsetup() +
-    ggtitle("IQR: Top outliers sorted by rank") 
-
-p4 <- gesd_outliers %>% 
-    ggsetup() +
-    ggtitle("GESD: Top outliers sorted by rank") 
-
-# Show plots
-p3
-p4
+# p3 <- iqr_outliers %>% 
+#     ggsetup() +
+#     ggtitle("IQR: Top outliers sorted by rank") 
+# 
+# p4 <- gesd_outliers %>% 
+#     ggsetup() +
+#     ggtitle("GESD: Top outliers sorted by rank") 
+# 
+# # Show plots
+# p3
+# p4
 
 # Experiecing issues
-# Possible solutins
+# Possible solutions
 # 1) consider changing factor to character
 # 2) sapply, lapply
+# 3) Try for loop
+
+for (i in hf_names_2)
+{
+    mydata2 %>%    # Twitter and GESD
+        time_decompose(Total_Value, method = "twitter", # The time series decomposition method. One of "stl" or "twitter". The
+                       # STL method uses seasonal decomposition (see decompose_stl()). The Twitter
+                       # method uses trend to remove the trend (see decompose_twitter()).
+                       frequency = "auto", # Controls the seasonal adjustment (removal of seasonality). Input can be either "auto", a time-based definition (e.g. "2 weeks"), or a numeric number of obser- vations per frequency (e.g.)
+                       # Consider frequency = "auto" or 12, however obtain more anomalies if 12
+                       trend = "auto", # Controls the trend component For stl, the trend controls the sensitivity of the lowess smoother, which is used to remove the remainder. 
+                       # For twitter, the trend controls the period width of the median, which are used to remove the trend and center the remainder.
+                       merge = TRUE) %>%  # A boolean. FALSE by default. If TRUE, will append results to the original data.
+        anomalize(remainder, method = "gesd", 
+                  alpha = 0.05,               # We can decrease alpha, which increases the bands making it more difficult to be an outlier. In reality, you’ll probably want to leave alpha in the range of 0.10 to 0.02, but it makes a nice illustration of how you can also use max_anoms to ensure only the most aggregious anomalies are identified.
+                  max_anoms = 0.20,           # the maximum percentage of data that can be an anomaly
+                  verbose = TRUE) %>%         # return a report of useful information related to the outliers
+        time_recompose() %>% 
+        #Anomaly Visualization
+        #plot_anomalies(ncol = 3, alpha_dots = 0.25) # this line of code plots without the grey area
+        plot_anomalies(time_recomposed = TRUE) #+ #, ncol = 3, alpha_dots = 0.25)
+    labs(title = "Time seriesd data - Twitter + GESD Method", x = "Time",
+         y = "Total outpatient visits", subtitle = "insert subtitle") 
+}
 
 
 
